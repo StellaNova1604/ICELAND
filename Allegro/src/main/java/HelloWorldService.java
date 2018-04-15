9bb201079b0d4377567bfbbb455da9f423ac49fd
Your Name <you@example.com>
2018-04-15 00:03:49 -0500

    Hello World REST Service with Parameters

--- Allegro/src/main/java/HelloWorldService.java
+++ Allegro/src/main/java/HelloWorldService.java
@@ -1,6 +1,12 @@
 
+import java.util.Collections;
+import java.util.Map;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/HelloWorld")
 public class HelloWorldService {
@@ -8,7 +14,16 @@ public class HelloWorldService {
 	@GET
 	@Path("/sayHello")
 	public String sayHello() {
-		return "<h1>Hello World from REST</h1>";
+		return "<h1>Hello World from REST in My Home</h1>";
 	}
-
+		
+	@GET
+	@Path("/sayHello/{name}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response sayHello(@PathParam("name") String name) {
+		Map<String, String> response = Collections.singletonMap(
+				"Message", "Hello " + name + "!");
+		return Response.ok(response).build();
+	}
+	
 }
